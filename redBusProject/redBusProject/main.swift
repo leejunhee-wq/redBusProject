8cb4eb80bc0003f763bac885dda7eb3c02ee8eb2
leejunhee-wq <gamg44011@gmail.com>
2025-09-12 15:56:28 +0900

    2일차
    1) 시간과 분 입력의 조건을 추가
    2) 막차끊긴 시간과, 이후 첫차까지 너무 많이 남은 시간들에 대해 새로운 출력 추가
    3) userTime은 변하지 않은 상수이니 let으로 변경
    4) 도착시간과 남은시간을 출력하기 위한 코드 작성

--- redBusProject/redBusProject/main.swift
+++ redBusProject/redBusProject/main.swift
@@ -7,12 +7,32 @@ guard let hour = readLine(), let userHour = Int(hour) else {
     print("숫자를 입력해주세요!")
     exit(0)
 }
+// 0 <= hour <= 23 조건 추가
+if userHour > 23 || userHour < 00 {
+    exit(0)
+}
+// 새벽시간 hour가 0~3이라면 첫차까지 시간이 너무 많이 남았습니다... 추가
+if userHour >= 0 && userHour < 4 {
+    print("첫차까지 시간이 너무 많이 남았습니다...")
+    exit(0)
+}
 
 print("몇분인가요? : ", terminator: "")
 guard let minute = readLine(), let userMinute = Int(minute) else {
+    
     print("숫자를 입력해주세요!")
     exit(0)
 }
+// 0 <= minute <= 59 조건 추가
+if userMinute > 59 || userMinute < 00 {
+    exit(0)
+}
+// 막차시간 이후 입력시 제한
+if userHour == 23 && userMinute > 42 {
+    print("막차가 지나갔습니다! 첫차를 기다려주세요!")
+    exit(0)
+}
+
 
 print("현재시각: \(userHour)시 \(userMinute)분")
 
@@ -26,11 +46,25 @@ let minuteChange = busArrivalTime.map {busTime -> Int in
     return busHour * 60 + busMinute
 }
 
-var userTime = userHour * 60 + userMinute
+let userTime = userHour * 60 + userMinute
+// minuteChange[]와 userTime의 차를 저장할 subUserTime이라는 배열 변수 추가
+var subUserTime = [Int]()
+for i in 0...43 {
+    subUserTime.append(minuteChange[i] - userTime)
+}
+// subUserTime에서 0보다 크면서 최솟값을 lowButUpZero라는 변수에 저장하여
+// 같은 index에 해당하는 busArrivalTime의 배열을 이용해 도착예정시간 출력
+if let lowButUpZero = subUserTime.filter({ $0 > 0 }).min() {
+    if let index = subUserTime.firstIndex(of: lowButUpZero){
+        let printBusTime = busArrivalTime[index]
+        print("도착예정시간: \(printBusTime.prefix(2)):\(printBusTime.suffix(2))")
+        print("\(lowButUpZero)분 후 도착합니다.")
+    }
+}
 
 
 
-    
+
 
 
 
